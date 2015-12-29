374b761b4e83ba2296110043c43b828c890fdb4c
Jillberth Estillore <ejillberth@gmail.com>
2015-12-29 14:39:07 +0800

    get profile

--- src/Foodcloud.php
+++ src/Foodcloud.php
@@ -35,8 +35,62 @@ class Foodcloud
         $this->curl = new cURL;
     }
 
+    /**
+     * Start Setters/Getters
+     */
     private function getUrl($path) {
         return $this->endpoint . $path;
+    }
+    public function setAccessToken($accessToken) {
+        $this->accessToken = $accessToken;
+        return $this;
+    }
+    public function getAccessToken() {
+        return $this->accessToken;
+    }
+    public function setEndpoint($endpoint) {
+        $this->endpoint = $endpoint;
+        return $this;
+    }
+    public function getEndpoint() {
+        return $this->endpoint;
+    }
+    public function getClientId() {
+        return $this->clientId;
+    }
+    public function setClientId($clientId) {
+        $this->clientId = $clientId;
+        return $this;
+    }
+    public function getClientSecret() {
+        return $this->clientSecret;
+    }
+    public function setClientSecret($clientSecret) {
+        $this->clientSecret = $clientSecret;
+        return $this;
+    }
+    public function getGrantType() {
+        return $this->grantType;
+    }
+    public function setGrantType($grantType) {
+        $this->grantType = $grantType;
+        return $this;
+    }
+    /**
+     * End Setters/Getters
+     */
+
+    private function request($method, $url, $data = []) {
+        $c = $this->curl->newRequest($method, $url, $data);
+        $c->setHeader('Authorization', $this->getAccessToken());
+        $res = $c->send();
+
+        $response = new Response;
+        $response->setSuccess($res->statusCode == 200);
+        $response->setStatus($res->statusCode);
+        $response->setData($res->body);
+
+        return $response;
     }
 
     /**
@@ -52,15 +106,17 @@ class Foodcloud
     }
 
     public function login($username, $password) {
-        $res = $this->curl->post($this->getUrl('/oauth/access_token'), [
+        return $this->request('post', $this->getUrl('/oauth/access_token'), [
             'grant_type' => $this->grantType,
             'client_id' => $this->clientId,
             'client_secret' => $this->clientSecret,
             'username' => $username,
             'password' => $password
             ]);
+    }
 
-        return json_decode($res);
+    public function getProfile() {
+        return $this->request('get', $this->getUrl('/profile'));
     }
 
 }
